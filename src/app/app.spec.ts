07029973a7f65588e0bbff9d8cf58bf7187c42d6
JR Jernival <267707087+zindien-hub@users.noreply.github.com>
2026-05-01 17:25:06 +0200

    chore: initialize angular frontend

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, datashare-frontend-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, datashare-frontend');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('datashare-frontend-app');
+  protected readonly title = signal('datashare-frontend');
 }
