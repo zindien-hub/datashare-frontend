1f06d33b52b028c7eb3146b00322f6d835e1dbf9
JR Jernival <267707087+zindien-hub@users.noreply.github.com>
2026-05-01 19:13:42 +0200

    feat: add initial auth pages and routing

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {}
